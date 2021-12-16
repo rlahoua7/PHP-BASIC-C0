f89534151ade92be308c97044ec3123fd8780b2d
rlahoua7 <2152116@talnet.nl>
2021-12-16 13:17:59 +0100

    opdracht 2.5

--- 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
+++ 2-Variabelen-en-datatype/2.4 - Datatype boolean & null/index.php
@@ -6,7 +6,7 @@
 $test = true;
 echo "de waarde van test is: " . $test . "<br>";
 $test = null;
-echo "de waarde van test is: " . $test . "<br>";
+echo "de waarde van test is: " . $test . "<br>";    
 ?>
 
 </body>
