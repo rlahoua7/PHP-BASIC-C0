d6d52fc0ce2ea991f1dbec20cd177a657d368085
rlahoua7 <2152116@talnet.nl>
2021-12-23 10:58:51 +0100

    opdracht 2.10

--- 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
+++ 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
@@ -7,10 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-    <?php>
+    <?php
      $voornaam = "Tom";
      $leeftijd = "19";
      echo "Jouw naam is " . $voornaam . " en je bent " . $leeftijd . " jaar oud.";
-    <?>
+    ?>
 </body>
-</html>
+</html> 
