6dde90271ff7be4ea379580604ed2ecc7cbef9e1
rlahoua7 <2152116@talnet.nl>
2021-12-23 11:16:26 +0100

    opdracht 3.1

--- 3-Arrays/3.1 - Array variabele/index.php
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -8,8 +8,11 @@
 </head>
 <body>
     <?php
-    $myMovies = array("the purge", "injustice", "cars");   
-    $myMovies = ["the purge", "injustice", "cars"];
+    $MyMovies = array("<li>Batman</li>" , "<li>Spiderman</li>" , "<li>The Joker</li>");
+
+    echo $MyMovies[0];
+    echo $MyMovies[1];  
+    echo $MyMovies[2]; 
     ?>
 </body>
 </html>
