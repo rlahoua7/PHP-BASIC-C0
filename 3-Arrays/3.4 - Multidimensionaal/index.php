19a54c19c165ecdaa9a2a730467b1f546cae0d01
rlahoua7 <2152116@talnet.nl>
2021-12-23 14:00:02 +0100

    opdracht 3.4

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$abcgetallen = [["24", "56", "78", "90"],[" a", " b", " c", " d"],];
+    echo ($abcgetallen [1][2]);
+    print_r ($abcgetallen [0][1]);
+    echo "<br>";
+    echo ($abcgetallen [1][2]);
+    print_r ($abcgetallen [0][3]);
+?>
+</body>
+</html>
---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $abcgetallen = array(" a", " b", " c", " d", ["24", "56", "78", "90"]);
+    print_r ($abcgetallen);
+    echo ($abcgetallen [0]);
+    echo ($abcgetallen [1][2]);
+    echo ($abcgetallen [2]);
+    echo ($abcgetallen [3][4]);
+
+    
+?>
+</body>
+</html>
